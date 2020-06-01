894087c90b542129e08d0d7aa284b14c93fe4a0b
mahmutemrahsari <mahmutemrahsari@gmail.com>
2020-06-01 19:52:46 +0200

    Array added! Multiple users can be registered!

--- src/main/java/com/example/demo/KundeController.java
+++ src/main/java/com/example/demo/KundeController.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class KundeController {
-    @GetMapping("/")
-    public Kunde returKunde(Kunde innKunde){
-       return innKunde;
+    public final List<Kunde>alleKunder= new ArrayList<>();
+
+    @PostMapping("/lagre")
+    public void lagreKunde(Kunde innKunde){
+        alleKunder.add(innKunde);
     }
+
+    @GetMapping("/hentAlle")
+    public List<Kunde> hentAlle(){
+       return alleKunder;
+    }
+
 }
